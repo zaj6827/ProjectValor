772b12236a1eb42d4ea165e9db5ffda3bd892b6d
ZacharyJohnson <zaj6827@gmail.com>
2017-08-03 16:16:43 -0700

    added cors npm package, xmlhttp request happening.

--- server.js
+++ server.js
@@ -3,7 +3,8 @@
 //TODO Do we need to set up the fs? I'm not sure if that's going to be necessary for this project?
 const express = require('express');
 const bodyParser = require('body-parser');
-const pg = require('pg')
+const pg = require('pg');
+const cors = require('cors');
 const PORT= process.env.PORT || 3000;
 const app = express();
 const conString = process.env.DATABASE_URL;
@@ -12,10 +13,17 @@ const client =  new pg.Client(conString);
 client.connect();
 client.on('error', err => console.error(err));
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+app.use(cors())
 //setting up middleware and setting static pages
 app.use(bodyParser.json());
 app.use(express.static('./public'));
+app.get('/products/:id', function (req, res) {
+  res.json({msg: 'This is CORS-enabled for all origins!'})
+})
 
+app.listen(80, function () {
+  console.log('CORS-enabled web server listening on port 80')
+})
 //routes
 app.get('/about', (req, res) => res.sendFile('about.html', {root: './public'}));
 app.get('/resources', (req, res) => res.sendFile('resources.html', {root: './public'}));
